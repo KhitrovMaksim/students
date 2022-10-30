b5691b998a78685ad918955536492b6e931ea844
Maksim <khitrov.maks@gmail.com>
2022-10-30 15:10:43 +0400

    Backend. Deploying. Testing env

--- backend/test.py
+++ backend/test.py
@@ -7,17 +7,17 @@ from fastapi import FastAPI, Depends
 TEST_SERVER_DB_PASSWORD = os.environ.get('TEST_SERVER_DB_PASSWORD')
 TEST_SERVER_HOST = os.environ.get('TEST_SERVER_HOST')
 
-SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://teacher:{TEST_SERVER_DB_PASSWORD}@127.0.0.1:3306/students"
-print("------------Start.Engine------------")
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://teacher:{TEST_SERVER_DB_PASSWORD}@localhost:3306/students"
+
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
 )
-print("------------Session Local------------")
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-print("------------Base------------")
+
 Base = declarative_base()
 
-print("------------Students------------")
+
 class Students(Base):
     __tablename__ = "students"
 
@@ -29,20 +29,18 @@ class Students(Base):
     phone = Column(String)
     favorite_sports = Column(String)
 
-print("------------app------------")
-app = FastAPI()
 print("------------Base------------")
 Base.metadata.create_all(bind=engine)
 
-print("------------get------------")
+app = FastAPI()
+
+
 def get_db():
     try:
-        print("------------try------------")
         db = SessionLocal()
-        print("------------yield------------")
         yield db
     finally:
-        print("------------finally------------")
+
         db.close()
 
 
