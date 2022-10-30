448dc00fd25f838e95f9f847d2d069fd2dd6e25b
Maksim <khitrov.maks@gmail.com>
2022-10-30 14:59:57 +0400

    Backend. Deploying. Testing env

--- backend/test.py
+++ backend/test.py
@@ -8,16 +8,16 @@ TEST_SERVER_DB_PASSWORD = os.environ.get('TEST_SERVER_DB_PASSWORD')
 TEST_SERVER_HOST = os.environ.get('TEST_SERVER_HOST')
 
 SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://teacher:{TEST_SERVER_DB_PASSWORD}@127.0.0.1:3306/students"
-
+print("------------Start.Engine------------")
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
 )
-
+print("------------Session Local------------")
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
+print("------------Base------------")
 Base = declarative_base()
 
-
+print("------------Students------------")
 class Students(Base):
     __tablename__ = "students"
 
@@ -29,25 +29,28 @@ class Students(Base):
     phone = Column(String)
     favorite_sports = Column(String)
 
-
+print("------------app------------")
 app = FastAPI()
-
+print("------------Base------------")
 Base.metadata.create_all(bind=engine)
 
-
+print("------------get------------")
 def get_db():
     try:
+        print("------------try------------")
         db = SessionLocal()
+        print("------------yield------------")
         yield db
     finally:
+        print("------------finally------------")
         db.close()
 
 
 @app.get("/")
 async def get_all_students():
-    return SQLALCHEMY_DATABASE_URL
+    return {"Test"}
 
 
-@app.get("/students")
-async def get_all_students(db: Session = Depends(get_db)):
-    return db.query(Students).all()
+# @app.get("/students")
+# async def get_all_students(db: Session = Depends(get_db)):
+#     return db.query(Students).all()
