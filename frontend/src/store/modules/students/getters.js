431d74d5784317fdc5735e018e4b69d352ee9897
Maksim <khitrov.maks@gmail.com>
2022-11-01 15:27:13 +0400

    Frontend. Added get request to backend.

--- frontend/src/store/modules/students/getters.js
+++ frontend/src/store/modules/students/getters.js
@@ -1,5 +1,26 @@
 export default {
-    students(state) {
+    async getStudents() {
+        try {
+            let link = ""
+            if (window.location.host === '127.0.0.1:8080' || window.location.host === 'localhost:8080')
+                link = "http://localhost:8000/students/"
+            else link = window.location.protocol + "//" + window.location.host + ":8080/students"
+            const response = await fetch(link, {
+                method: 'GET'
+            })
+
+            if (!response.ok) {
+                throw new Error(`Error! status: ${response.status}`);
+            }
+
+            const result = await response.json();
+            return result;
+        } catch (err) {
+            console.log(err);
+        }
+    },
+    students(state, getters) {
+        console.log(getters.getStudents)
         return state.students;
     },
     hasStudents(state) {
